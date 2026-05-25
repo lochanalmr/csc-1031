f7bf409d5a8c6cb5bc963d3c272412b1765e4e66
Lochana Ranatunga <161409757+lochanalmr@users.noreply.github.com>
2026-05-25 09:28:39 +0530

    created calculator program

---
+++ calculator/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Calculator\n\n");
+
+    printf("Menu:\n");
+    printf("+: Addition\n");
+    printf("-: Subtraction\n");
+    printf("*: Multiplication\n");
+    printf("/: Division\n");
+
+    char operation = '\0';
+    printf("Insert operation: ");
+    scanf("%c", &operation);
+
+    if (operation != '+' && operation != '-' && operation != '*' && operation != '/'){
+        printf("Invalid operation!\n");
+        return 1;
+    }
+
+    float number1 = 0.0;
+    printf("\nInsert Number 1: ");
+    scanf("%f", &number1);
+
+    float number2 = 0.0;
+    printf("Insert Number 2: ");
+    scanf("%f", &number2);
+
+    float answer = 0.0;
+    if (operation == '+'){
+        answer = number1 + number2;
+    }
+    else if (operation == '-'){
+        answer = number1 - number2;
+    }
+    else if (operation == '*'){
+        answer = number1 * number2;
+    }
+    else{
+        answer = number1 / number2;
+    }
+    printf("%.2f %c %.2f = %.2f\n", number1, operation, number2, answer);
+    return 0;
+}
